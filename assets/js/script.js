5f1d7a1b0a52295f9d6d729eac0b15cd36a7a961
Nathan Sanford <nwsanfor24@gmail.com>
2020-02-20 14:08:48 -0500

    add more code and animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
     $('.parallax').parallax();
+
+    VANTA.BIRDS({
+        el: "#vantajs",
+        mouseControls: true,
+        touchControls: true,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        backgroundColor: 0xb1625,
+        color1: 0x5cff,
+        color2: 0x2eca48
+    });
+
+    document.cookie = 'cross-site-cookie=bar; SameSite=None; Secure';
+    document.cookie = 'same-site-cookie=foo; SameSite=Lax';
 });
+
